309b78658a2ce4df4abc8050ac6bf5d9f3bde084
Allen zhang <wdzxl198@163.com>
2023-05-11 14:51:45 +0800

    updating site on Thu May 11 14:51:44 CST 2023

--- deployoss.sh
+++ deployoss.sh
@@ -1,5 +1,15 @@
 echo "start hugo build"
 hugo --minify -D --gc --debug
 
+echo "---------- end hugo build -------------"
+
+echo "start push github"
+
+sh ./push.sh
+
+echo "--------- end git push -------------"
+
 echo "start copy public to oss"
 ossutil cp -ru ./public oss://hugo-site/
+
+echo "-------- end oss copy ------------""
